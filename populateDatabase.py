c6980ea695050bdbf233d035c5b7fd566007174d
AmishKakka <amishkakka@gmail.com>
2025-04-07 14:04:27 -0700

    Creation of a single central DB. Each new embedding will be added to this single DB.

--- populateDatabase.py
+++ populateDatabase.py
@@ -1,10 +1,11 @@
 from google import genai
 from google.genai import types
 import langchain_chroma as chromaDB
+from pprint import pprint
 
 class GooglePalmEmbeddings:
     def __init__(self):
-        API_KEY = "your_api_key"
+        API_KEY = "AIzaSyABaEnflvVQD1TCxCWKDoomIM63jLHrdcs"
         self.client = genai.Client(api_key=API_KEY)
         
     def embed_documents(self, documents):
@@ -23,18 +24,16 @@ class GooglePalmEmbeddings:
 
 
 class ChromaDB:
-    def __init__(self, filename, embeddingFunction):
-        self.filename = filename
-        self.vector_db = chromaDB.Chroma(persist_directory=f'vectorData/{self.filename}', 
+    def __init__(self, embeddingFunction):
+        self.vector_db = chromaDB.Chroma(persist_directory=f'vectorData/', 
                                 embedding_function=embeddingFunction)
 
     def addEmbeddings_to_Chroma(self, chunks):
         doc_name = chunks[0].metadata.get("source")
-        print("Adding embeddings to ChromaDB collection for : ", doc_name)
-
         existing_items = self.vector_db.get(include=[])
         existing_ids = set(existing_items["ids"])
         print(f"Number of existing documents in DB: {len(existing_ids)}")
+        print("Adding embeddings to ChromaDB collection for : ", doc_name)
         
         new_chunks = []
         for chunk in chunks:
@@ -43,13 +42,12 @@ class ChromaDB:
         if len(new_chunks) != 0:
             chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
             self.vector_db.add_documents(new_chunks, ids=chunk_ids)
-            print(f"Added {len(new_chunks)} new documents to database {self.filename}.")
+            print(f"Added {len(new_chunks)} new documents.")
             print("Added embeddings to ChromaDB.")
         else:
             print("No new documents to add.")
             
     def observeDB(self):
-        print(f"Number of embeddings for document '{self.filename}' --> {len(self.vector_db.get(include=[])["ids"])}")
         print("Document IDs of embeddings in database: ", self.vector_db.get(include=[])["ids"])
     
     
@@ -57,10 +55,21 @@ class ChromaDB:
 # from processDocument import load_file_and_split, createChunkID
 
 # # Loading the file and splitting it into chunks
-# split_docs = load_file_and_split(r"./assets/name_of_the_document.pdf")
-# chunks = createChunkID(split_docs)
+# split_docs = load_file_and_split(r"./assets/AttentionPaper.pdf")
+# chunks1 = createChunkID(split_docs)
 
-# addEmbeddings_to_Chroma(chunks)
+# split_docs = load_file_and_split(r"./assets/other_document.pdf")
+# chunks2 = createChunkID(split_docs)
 
-# observeDB("name_of_the_document")
+# chunks = chunks1 + chunks2
+# print("Number of chunks: \n", len(chunks))
+
+# # Creating the embeddings and adding them to the ChromaDB
+# db = ChromaDB(GooglePalmEmbeddings())
+
+# for i in range(0, len(chunks), 100):
+#     # print(f"Adding chunks {i} to {i+99}")
+#     db.addEmbeddings_to_Chroma(chunks[i:i+100])
+
+# db.observeDB()
 # ========================================================= #
